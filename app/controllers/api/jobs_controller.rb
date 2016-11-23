faf2ec899d265d7858bfd393e8956844c09cb33e
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2016-11-23 15:57:45 -0800

    Orders jobs in descending

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,6 +1,6 @@
 class API::JobsController < API::BaseController
     def index
-        @entries = Entry.all.limit(100)
+        @entries = Entry.all.order('published_at DESC').limit(100)
         render json: @entries
     end
 end
