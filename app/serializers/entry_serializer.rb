725ecf4ee859803ce1abcf7e92a25f13e6d6038a
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2016-11-23 18:48:04 -0800

    Adds some margin

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,7 +1,11 @@
 class EntrySerializer < ActiveModel::Serializer
-    attributes :id, :title, :published_at, :favicon_url, :url
+    attributes :id, :title, :published_at, :favicon_url, :url, :source_name
 
     def favicon_url
         object.source.favicon_url
+    end
+
+    def source_name
+        object.source.name
     end
 end
--- assets/javascripts/components/JobListItem.js
+++ assets/javascripts/components/JobListItem.js
@@ -2,14 +2,36 @@ import React from 'react';
 import moment from 'moment';
 import Avatar from 'material-ui/Avatar';
 import {ListItem} from 'material-ui/List';
+import {capitalize} from 'lodash';
 
 export default class extends React.Component {
     render() {
         return (
             <ListItem
-                leftAvatar={<Avatar src={this.props.job.get('favicon_url')} />}
+                leftAvatar={this.getAvatar()}
                 primaryText={this.props.job.get('title')}
-                secondaryText={moment(this.props.job.get('published_at')).fromNow()} />
+                secondaryText={this.getSecondaryText()} />
         );
+    }
+
+    hasFavicon() {
+        return !!this.props.job.get('favicon_url');
+    }
+
+    getAvatar() {
+        if (this.hasFavicon()) {
+            return (<Avatar backgroundColor='#ffffff' src={this.props.job.get('favicon_url')} />);
+        }
+        else {
+            return (<Avatar backgroundColor='#ffffff' color='#000'>{this.getAvatarLetter()}</Avatar>);
+        }
+    }
+
+    getAvatarLetter() {
+        return capitalize(this.props.job.get('source_name')[0]);
+    }
+
+    getSecondaryText() {
+        return moment(this.props.job.get('published_at')).fromNow() + " on " + this.props.job.get('source_name');
     }
 }
