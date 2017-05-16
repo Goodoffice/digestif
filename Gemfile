c3f7431aae559bd7cb76c73e6be59552c0d545c9
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2017-05-16 08:49:40 -0700

    Upgrades to Rails 5

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'https://rubygems.org'
 
 
-gem 'rails', '4.2.5.2'
+gem 'rails', '5.1.1'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'compass-rails'
 gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module Digestif
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.precompile << "email.css"
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
